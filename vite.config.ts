81cf90bd38f150e2dc7c54d787d7b3cdd55002b8
Philippe Loctaux <p@philippeloctaux.com>
2026-05-14 02:06:07 +0200

    add vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [solid(), tailwindcss()],
+  plugins: [
+    solid(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+    }),
+  ],
 });
